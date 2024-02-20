a23a9c1b0f20b01fc7ec938ade7774a1c87ee6a7
MiguelAngelfnc <miguelmb3112@gmail.com>
2024-02-20 16:26:57 +0100

    ejercicioProps

---
+++ src/Components/Age.jsx
@@ -0,0 +1,7 @@
+export function Age({age}){
+return(
+    <div>
+        <p>your age is {age}</p>
+    </div>
+)
+}
---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import { HelloWorld } from "./HelloWorld";
+import { Welcome } from "./Welcome";
+
+export function App(){
+    return(
+        <div>
+          <h1>My fantastic Application!</h1>
+          <hr />
+          <HelloWorld/>
+          <hr />
+          <Welcome name={"José"} age={40} />
+        </div>
+    )
+}
